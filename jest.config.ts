9127b2ac4aa9b4591ceb17c4989981fe8dd8d82b
juanfelixk <you@example.com>
2026-03-05 20:24:09 +0700

    added complete features

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,29 @@
-const { TestEnvironment } = require("jest-environment-jsdom");
-const nextJest = require("next/jest");
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-    dir: "./",
+  dir: "./",
 });
 
-const customJestConfig = {
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-    testEnvironment: "jest-environment-jsdom",
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
+  testMatch: ["**/__tests__/**/*.test.[jt]s?(x)"],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  collectCoverageFrom: [
+    "app/**/*.{ts,tsx}",
+    "lib/**/*.{ts,tsx}",
+    "components/**/*.{ts,tsx}",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+    "!**/.next/**",
+  ],
+  coverageDirectory: "coverage",
+  coverageReporters: ["text", "lcov", "html"],
 };
 
-module.exports = createJestConfig(customJestConfig)
+export default createJestConfig(config);
